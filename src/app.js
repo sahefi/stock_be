1d2e3362b78b6af94dbb19a77491d92d49539130
Alvianto Putra Sahefi <sahefiputra@gmail.com>
2025-08-03 17:34:22 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler'); 
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
